7694b2cf04643b936ac06a9dc6512ccfb497080d
qiayue <qiayue@gmail.com>
2024-04-16 15:19:27 +0800

    update seo meta

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# 2xt
+# (Image.ToText.io)[https://image.totext.io/]
 
-Image to text, fast. Built with the [Vercel AI SDK](https://sdk.vercel.ai), [Claude](https://claude.ai/), and [Next.js](https://nextjs.org).
+This project is based on (2txt)[https://github.com/ai-ng/2txt] by (ai-ng)[ttps://github.com/ai-ng/2txt].
+Original Project can be found at (https://github.com/ai-ng/2txt)[https://github.com/ai-ng/2txt].
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fai-ng%2F2txt&env=ANTHROPIC_API_KEY&envDescription=Your%20Anthropic%20API%20key%20from%20https%3A%2F%2Fconsole.anthropic.com&envLink=https%3A%2F%2Fconsole.anthropic.com%2F&project-name=2txt&repository-name=2txt&demo-title=2txt&demo-description=Image%20to%20text%2C%20fast.&demo-url=https%3A%2F%2F2txt.vercel.app&demo-image=https%3A%2F%2F2txt.vercel.app%2Fopengraph-image.png)
+(Image to text)[https://image.totext.io/], with AI. Built with the [Vercel AI SDK](https://sdk.vercel.ai), [Claude](https://claude.ai/), and [Next.js](https://nextjs.org).
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fqiayue%2Fimage.totext.io&env=ANTHROPIC_API_KEY&envDescription=Your%20Anthropic%20API%20key%20from%20https%3A%2F%2Fconsole.anthropic.com&envLink=https%3A%2F%2Fconsole.anthropic.com%2F&project-name=image2txt&repository-name=image2txt&demo-title=image2txt&demo-description=Image%20to%20text%2C%20with%20AI.&demo-url=https%3A%2F%2Fimage.totext.io%2F)
 
 ## Developing
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-	title: "2txt",
-	description: "Image to text, fast.",
+	title: "Image.ToText.io: Free Image to text, with AI",
+	description: "Free Image to text, with AI.",
 };
 
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
 				className={`${GeistSans.variable} ${GeistMono.variable} font-mono bg-gray-50 dark:bg-gray-950 text-black dark:text-white px-3 lg:px-10 py-4 lg:py-10 min-h-dvh flex flex-col`}
 			>
 				<h1 className="font-semibold text-center text-2xl bg-gradient-to-b dark:from-gray-50 dark:to-gray-200 from-gray-950 to-gray-800 bg-clip-text text-transparent select-none">
-					2txt
+					Image.ToText.io: Free Image to text
 				</h1>
 
 				<main className="grow flex flex-col lg:flex-row gap-6 py-4 lg:py-10">
@@ -30,8 +30,8 @@ export default function RootLayout({
 
 				<footer className="lg:flex flex-row justify-between text-center text-sm dark:text-gray-400 text-gray-600 select-none">
 					<p>
-						<A href="https://github.com/ai-ng">ai-ng</A> /{" "}
-						<A href="https://nickoates.com">nick oates</A>
+						<A href="https://image.totext.io/">Image.ToText.io</A> /{" "}
+						<A href="https://image.totext.io/">Free Image to text</A>
 					</p>
 					<p>
 						Built with{" "}
@@ -39,9 +39,9 @@ export default function RootLayout({
 						<A href="https://claude.ai/">Claude</A>
 					</p>
 					<p>
-						<A href="https://github.com/ai-ng/2txt">source</A> /{" "}
-						<A href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fai-ng%2F2txt&env=ANTHROPIC_API_KEY&envDescription=Your%20Anthropic%20API%20key%20from%20https%3A%2F%2Fconsole.anthropic.com&envLink=https%3A%2F%2Fconsole.anthropic.com%2F&project-name=2txt&repository-name=2txt&demo-title=2txt&demo-description=Image%20to%20text%2C%20fast.&demo-url=https%3A%2F%2F2txt.vercel.app&demo-image=https%3A%2F%2F2txt.vercel.app%2Fopengraph-image.png">
-							deploy
+						<A href="https://github.com/qiayue/image.totext.io">Source</A> /{" "}
+						<A href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fqiayue%2Fimage.totext.io&env=ANTHROPIC_API_KEY&envDescription=Your%20Anthropic%20API%20key%20from%20https%3A%2F%2Fconsole.anthropic.com&envLink=https%3A%2F%2Fconsole.anthropic.com%2F&project-name=image2txt&repository-name=image2txt&demo-title=image2txt&demo-description=Image%20to%20text%2C%20with%20AI.&demo-url=https%3A%2F%2Fimage.totext.io%2F">
+							Deploy
 						</A>
 					</p>
 				</footer>
